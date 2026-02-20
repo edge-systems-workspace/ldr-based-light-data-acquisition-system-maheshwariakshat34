2655b36cf36d6582912cab578e143b90d419c18d
maheshwariakshat34 <akshat.lakhotiya_cs.aiml24@gla.ac.in>
2026-02-20 15:08:38 +0530

    define LDR analog pin (A3)

--- src/main.cpp
+++ src/main.cpp
@@ -2,43 +2,20 @@
 /**
  * @file main.ino
  * @brief Embedded Light Intensity Monitoring using LDR
- * @author YOUR_NAME
- * @date YYYY-MM-DD
+ * @author Akshat Lakhotiya
+ * @date 2026-02-20
  *
  * @details
  * Reads analog brightness data from LDR sensor
  * and displays structured output via Serial Monitor.
  */
+#define ldrPin A3
 
- // TODO 1:
- // Define LDR analog pin (Use A0)
-
- // TODO 2:
- // Create variable to store sensor reading
 
 void setup() {
 
-    // TODO 3:
-    // Initialize Serial communication (9600 baud rate)
-
-    // TODO 4:
-    // Print system initialization message
 }
 
 void loop() {
 
-    // TODO 5:
-    // Read analog value from LDR
-
-    // TODO 6:
-    // Print raw ADC value
-
-    // TODO 7:
-    // Apply threshold logic (Bright / Dark detection)
-
-    // TODO 8:
-    // Print brightness status
-
-    // TODO 9:
-    // Add delay (500ms or 1 second)
 }
